b88e34a2d064e472a0a121b137b7394f74d056df
drr00t <adribeiro@gmail.com>
2018-07-14 22:29:16 -0300

    added an one level example of  threshould fulfillment

--- test/smartcontractsSpec.js
+++ test/smartcontractsSpec.js
@@ -14,16 +14,60 @@ describe("SmartContracts", function(){
         });
 
         it("Ed25519Sha256 condition", function(){
-            let msg = Buffer.from('dados enviados');
+            const msg = Buffer.from('dados enviados');
+            const msg2 = Buffer.from('dados enviados diferentes');
 
             let keyPair = sc.generateKeys();
             const ed25519_ff = new cc.Ed25519Sha256();
             ed25519_ff.sign(msg, keyPair.privateKey);
+            const valid = ed25519_ff.validate(msg);
+
             console.log("condition: %s",ed25519_ff.getConditionUri());
             console.log("fulfillment: %s",ed25519_ff.serializeUri());
+            console.log("fulfillment validate the condition: %s", ed25519_ff.validate(msg));
 
             expect(keyPair).to.have.a.property("publicKey");
             expect(keyPair).to.have.a.property("privateKey");
+            expect(ed25519_ff.validate(msg)).to.be.true;
+            expect(()=> {return ed25519_ff.validate(msg2)}).to.throw();
+           
+        });
+
+        it("THRESHOLD-SHA-256", function(){
+            const msg1 = Buffer.from('dados enviados');
+            const msg2 = Buffer.from('dados enviados diferentes');
+
+            let keyPair1 = sc.generateKeys();
+            let keyPair2 = sc.generateKeys();
+
+            const ed25519_ff_1 = new cc.Ed25519Sha256();
+            const ed25519_ff_2 = new cc.Ed25519Sha256();
+
+            ed25519_ff_1.sign(msg1, keyPair1.privateKey);
+            ed25519_ff_2.sign(msg1, keyPair2.privateKey);
+
+            const ok_threshold_ff_t1 = new cc.ThresholdSha256();
+            ok_threshold_ff_t1.addSubfulfillmentUri(ed25519_ff_1.serializeUri());
+            ok_threshold_ff_t1.setThreshold(1);
+
+            console.log("condition: %s",ok_threshold_ff_t1.getConditionUri());
+            console.log("fulfillment: %s",ok_threshold_ff_t1.serializeUri());
+
+            expect(ok_threshold_ff_t1.validate(msg1)).to.be.true;
+            expect(()=> {return ok_threshold_ff_t1.validate(msg2);}).to.throw();
+
+            // ====================================================================
+
+            const ok_threshold_ff_t2 = new cc.ThresholdSha256();
+            ok_threshold_ff_t2.addSubfulfillmentUri(ed25519_ff_1.serializeUri());
+            ok_threshold_ff_t2.addSubfulfillmentUri(ed25519_ff_2.serializeUri());
+            ok_threshold_ff_t2.setThreshold(2);
+
+            console.log("condition: %s",ok_threshold_ff_t2.getConditionUri());
+            console.log("fulfillment: %s",ok_threshold_ff_t2.serializeUri());
+
+            expect(ok_threshold_ff_t2.validate(msg1)).to.be.true;
+            expect(()=> {return ok_threshold_ff_t2.validate(msg2);}).to.throw();
         });
     }); 
 });
