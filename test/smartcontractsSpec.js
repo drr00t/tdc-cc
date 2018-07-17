4e3d4d9721e529c7e64f30095f8be5495b468f97
drr00t <adribeiro@gmail.com>
2018-07-17 01:21:56 -0300

    multiassinaturas com Threashould

--- test/smartcontractsSpec.js
+++ test/smartcontractsSpec.js
@@ -33,7 +33,7 @@ describe("SmartContracts", function(){
            
         });
 
-        it("THRESHOLD-SHA-256", function(){
+        it("THRESHOLD-SHA-256 - Multiassinaturas", function(){
             const msg1 = Buffer.from('dados enviados');
             const msg2 = Buffer.from('dados enviados diferentes');
 
@@ -68,6 +68,92 @@ describe("SmartContracts", function(){
 
             expect(ok_threshold_ff_t2.validate(msg1)).to.be.true;
             expect(()=> {return ok_threshold_ff_t2.validate(msg2);}).to.throw();
+        });
+
+        it("THRESHOLD-SHA-256 - Multiassinaturas multi-nível", function(){
+            const msg1 = Buffer.from('dados enviados');
+            const msg2 = Buffer.from('dados enviados diferentes');
+
+            let keyPair1 = sc.generateKeys();
+            let keyPair2 = sc.generateKeys();
+
+            const ed25519_ff_1 = new cc.Ed25519Sha256();
+            const ed25519_ff_2 = new cc.Ed25519Sha256();
+            const ed25519_ff_3 = new cc.Ed25519Sha256();
+            
+            const sub_threshold_ff = new cc.ThresholdSha256();
+            ed25519_ff_3.sign(msg1, keyPair1.privateKey);
+
+            sub_threshold_ff.addSubfulfillmentUri(ed25519_ff_3.serializeUri());
+            sub_threshold_ff.setThreshold(1);
+
+
+            ed25519_ff_1.sign(msg1, keyPair1.privateKey);
+            ed25519_ff_2.sign(msg1, keyPair2.privateKey);
+
+            const ok_threshold_ff_t1 = new cc.ThresholdSha256();
+            ok_threshold_ff_t1.addSubfulfillmentUri(ed25519_ff_1.serializeUri());
+            ok_threshold_ff_t1.addSubfulfillmentUri(sub_threshold_ff.serializeUri());
+            ok_threshold_ff_t1.setThreshold(2);
+
+            console.log("condition: %s",ok_threshold_ff_t1.getConditionUri());
+            console.log("fulfillment: %s",ok_threshold_ff_t1.serializeUri());
+
+            expect(ok_threshold_ff_t1.validate(msg1)).to.be.true;
+            expect(()=> {return ok_threshold_ff_t1.validate(msg2);}).to.throw();
+
+            // ====================================================================
+
+            const ok_threshold_ff_t2 = new cc.ThresholdSha256();
+            ok_threshold_ff_t2.addSubfulfillmentUri(ed25519_ff_1.serializeUri());
+            ok_threshold_ff_t2.addSubfulfillmentUri(ed25519_ff_2.serializeUri());
+            ok_threshold_ff_t2.setThreshold(2);
+
+            console.log("condition: %s",ok_threshold_ff_t2.getConditionUri());
+            console.log("fulfillment: %s",ok_threshold_ff_t2.serializeUri());
+
+            expect(ok_threshold_ff_t2.validate(msg1)).to.be.true;
+            expect(()=> {return ok_threshold_ff_t2.validate(msg2);}).to.throw();
+        });
+
+        it("PREFIX-SHA-256 - Multiassinaturas com escopo", function(){
+            const msg1 = Buffer.from('dados enviados');
+            const msg2 = Buffer.from('dados enviados diferentes');
+
+            let keyPair1 = sc.generateKeys();
+
+            const ed25519_ff_1 = new cc.Ed25519Sha256();
+            const ed25519_ff_2 = new cc.Ed25519Sha256();
+
+            ed25519_ff_1.sign(msg1, keyPair1.privateKey);
+            ed25519_ff_2.sign(msg2, keyPair1.privateKey);
+
+            const prefix_ff_t1 = new cc.PrefixSha256();
+            prefix_ff_t1.setPrefix(Buffer.from('PASSO UM:'))
+            prefix_ff_t1.setSubfulfillmentUri(ed25519_ff_1.serializeUri());
+            prefix_ff_t1.setMaxMessageLength(900);
+
+            const prefix_ff_t2 = new cc.PrefixSha256();
+            prefix_ff_t2.setPrefix(Buffer.from('PASSO DOIS:'))
+            prefix_ff_t2.setSubfulfillmentUri(ed25519_ff_2.serializeUri());
+            prefix_ff_t2.setMaxMessageLength(1000);
+            
+            // ====================================================================
+
+            const ok_threshold_ff_t2 = new cc.ThresholdSha256();
+            ok_threshold_ff_t2.addSubfulfillmentUri(prefix_ff_t1.serializeUri());
+            ok_threshold_ff_t2.addSubfulfillmentUri(prefix_ff_t2.serializeUri());
+            ok_threshold_ff_t2.setThreshold(1);
+
+            console.log("condition: %s",ok_threshold_ff_t2.getConditionUri());
+            console.log("fulfillment: %s",ok_threshold_ff_t2.serializeUri());
+            
+            expect(prefix_ff_t1.validate(msg1)).to.be.true;
+            expect(prefix_ff_t2.validate(msg2)).to.be.true;
+
+            expect(ok_threshold_ff_t2.validate(msg1)).to.be.true;
+            expect(ok_threshold_ff_t2.validate(msg2)).to.be.true;
+
         });
     }); 
 });
